fb8d1af477f11ff9a49f2489356cfe21e298a2f5
Augus <541468099@qq.com>
2018-05-22 16:09:55 +0800

    add version2

--- log.md
+++ log.md
@@ -2,5 +2,8 @@
 [version1](#version1)
 
 <span id = "version1"></span>
-## version1(2018.5.21)
-使用双层DQN读取特征
+## version1
+**(2018.5.21)**： 使用随机策略作为版本1提交，得到分数1300左右。
+## version2
+**(2018.5.21)**： 使用双层DQN读取特征，初步提交失败，没有结果。
+**(2018.5.22)**： 本地测试可运行，但是速度缓慢，大概1小时2个eposide。查阅文档知docker需要pull下tensorflow，pull之后重新提交。
--- sonic_RL.py
+++ sonic_RL.py
@@ -7,7 +7,7 @@ from collections import deque
 from retro_contest.local import make
 
 EPISODE = 10000  # Episode limitation
-STEP = 300  # Step limitation in an episode
+STEP = 1000  # Step limitation in an episode
 TEST = 10
 
 GAMMA = 0.9  # discount factor for target Q
@@ -99,21 +99,21 @@ class DQN:
         h_conv1 = tf.nn.relu(self.conv2d(self.state_input, w_conv1) + b_conv1)
         h_pool1 = self.max_pool_2x2(h_conv1)
         # 卷积2
-        w_conv2 = self.weight_variable([5, 5, 7, 49])
-        b_conv2 = self.bias_variable([49])
+        w_conv2 = self.weight_variable([5, 5, 7, 14])
+        b_conv2 = self.bias_variable([14])
         h_conv2 = tf.nn.relu(self.conv2d(h_pool1, w_conv2) + b_conv2)
         h_pool2 = self.max_pool_2x2(h_conv2)
         # hidden layers
-        input_dim = int(self.state_dim_row / 4) * int(self.state_dim_col / 4) * 49
-        w_fc1 = self.weight_variable([input_dim, 49])
-        b_fc1 = self.bias_variable([49])
+        input_dim = int(self.state_dim_row / 4) * int(self.state_dim_col / 4) * 14
+        w_fc1 = self.weight_variable([input_dim, 14])
+        b_fc1 = self.bias_variable([14])
         h_pool2_flat = tf.reshape(h_pool2, [-1, input_dim])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
         # DropOut
         self.keep_prob = tf.placeholder("float")
         h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
         # 输出层
-        w_fc2 = self.weight_variable([49, self.action_dim])
+        w_fc2 = self.weight_variable([14, self.action_dim])
         b_fc2 = self.bias_variable([self.action_dim])
         y_conv = tf.matmul(h_fc1_drop, w_fc2) + b_fc2
         # Q Value layer
@@ -150,7 +150,6 @@ class DQN:
             self.replay_buffer.popleft()
 
         if len(self.replay_buffer) > BATCH_SIZE:
-            print("begin train")
             self.train_q_network()
 
     def train_q_network(self):
@@ -212,6 +211,7 @@ def main():
         state = np.array(env.reset())
         # Train
         for step in range(STEP):
+            print('episode:  %d,  step:  %d' % (episode, step))
             action = agent.egreedy_action(state)  # e-greedy action for train
             print(action)
             next_state, reward, done, _ = env.step(action)
